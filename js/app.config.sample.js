da916ae884c039d6182d3be689ed8eb0c460a61a
Tim Winselmann <tim@Tim-MBP.fritz.box>
2017-01-08 19:59:09 +0100

    added ical support

--- js/app.config.sample.js
+++ js/app.config.sample.js
@@ -1,7 +1,17 @@
-var lightmanager = angular.module('magicMirrorApp');
+avar lightmanager = angular.module('magicMirrorApp');
 
 // configuration for http://www.openweathermap.org
 lightmanager.constant('openweathermap_config', {
   locationID: '', // openweathermap cityCode
   appid: '' // openweathermap appid
+});
+
+// calendar configuration
+lightmanager.constant('calendar_config', {
+  // TODO add colors for each ical url
+  ical_urls: [
+    '', // ical url 1
+    '', // ical url 2
+    '', // ical url ...
+    ]
 });
---
+++ js/calendarCtrl.js
@@ -0,0 +1,110 @@
+var magicMirrorApp = angular.module('magicMirrorApp');
+
+magicMirrorApp.controller("CalendarCtrl", function($scope, $http, $interval, $log, calendar_config) {
+
+  $scope.events = [];
+
+  for (var i = 0; i < calendar_config.ical_urls.length; i++) {
+    $http.get(calendar_config.ical_urls[i]).then(function(response) {
+      var now = ICAL.Time.now();
+      parseIcal(response.data, now);
+  	}, function(response) {
+  		$log.error(response);
+    });
+  }
+
+  var parseIcal = function (icalData, date) {
+    var jcalData = ICAL.parse(icalData);
+    var comp = new ICAL.Component(jcalData);
+
+    var events = comp.getAllSubcomponents('vevent');
+
+    var timezoneComp = comp.getFirstSubcomponent('vtimezone');
+    var tzid = timezoneComp.getFirstPropertyValue('tzid');
+    var timezone = new ICAL.Timezone({
+      component: timezoneComp,
+      tzid
+    });
+
+    var now = new Date();
+
+    for (e in events) {
+      var event = new ICAL.Event(events[e]);
+
+      if (event.isRecurring()) {
+
+        var title = event.summary || false;
+        var endDate = event.endDate.toJSDate();
+
+        var recurringLimit = 0;
+        var expand = event.iterator();
+
+        while (next = expand.next()) {
+          var startDate = next.toJSDate();
+          if (startDate.withoutTime() == now.withoutTime()) {
+
+            $scope.events.push({
+              'title': title,
+              'startDate': startDate,
+              'startsToday': event.duration.days < 1,
+              'endDate': endDate,
+              'endsToday': event.duration.days < 1,
+            });
+
+          }
+
+          /* handle infinite rules: limit iterations */
+          if (recurringLimit == 100) {
+            $log.debug('Reached recurring limit for ' + title);
+            break;
+          }
+          recurringLimit++;
+        }
+
+      } else {
+
+        // TODO Test or-initialization
+        var title = event.summary || false;
+        var startDate = event.startDate.toJSDate();
+        var endDate = event.endDate.toJSDate();
+
+        if (endDate.withoutTime() < now.withoutTime()) {
+          //$log.debug('Event ends in the past: ' + title);
+          continue;
+        } else if (startDate.withoutTime() > now.withoutTime()) {
+          //$log.debug('Event starts in the future: ' + title);
+          continue;
+        } else if (startDate.withoutTime() <= now.withoutTime()) {
+          //$log.debug('Event: ' + title, event);
+
+          // TODO add class 'deactivated' if event finished
+
+          $scope.events.push({
+            'title': title,
+            'startDate': startDate,
+            'startsToday': startDate.withoutTime() == now.withoutTime(),
+            'endDate': endDate,
+            'endsToday': endDate.withoutTime() == now.withoutTime(),
+          });
+
+        }
+      }
+    }
+  }
+
+});
+
+magicMirrorApp.component('calendarDetail', {
+  templateUrl: 'components/calendarDetails.html',
+  bindings: {
+    events: '='
+  }
+});
+
+/* extend Date prototype: withoutTime() returns a new Date instance
+ * without time information in unix time format */
+Date.prototype.withoutTime = function () {
+    var d = new Date(this);
+    d.setHours(0, 0, 0, 0, 0);
+    return d.getTime();
+}
